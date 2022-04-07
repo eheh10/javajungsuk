ffde6018f3a2bf278db28b5c4f35dba60258ba31
eunhye park <lime1pm@naver.com>
2022-04-08 02:52:37 +0900

    Feat: Add 11-23 exercise

---
+++ src/ch11/HashSetEx3.java
@@ -0,0 +1,30 @@
+package ch11;
+
+import java.util.HashSet;
+
+class HashSetEx3 {
+	public static void main(String[] args) {
+		HashSet set = new HashSet();
+
+		set.add("abc");
+		set.add("abc");
+		set.add(new Person("David",10));
+		set.add(new Person("David",10)); // 위와 주소값이 다른 별개의 인스턴스이므로 중복으로 간주되지 않는다
+
+		System.out.println(set);
+	}
+}
+
+class Person {
+	String name;
+	int age;
+
+	Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString() {
+		return name +":"+ age;
+	}
+}
