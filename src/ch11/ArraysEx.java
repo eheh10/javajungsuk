a5481219dcd49745a09be3adddfa52e85618bcd9
eunhye park <lime1pm@naver.com>
2022-04-08 02:51:09 +0900

    Feat: Add 11-18 exercise

---
+++ src/ch11/ArraysEx.java
@@ -0,0 +1,54 @@
+package ch11;
+
+import java.util.Arrays;
+
+class ArraysEx {
+	public static void main(String[] args) {
+		int[]	arr   =  {0,1,2,3,4};
+		int[][] arr2D =  {{11,12,13}, {21,22,23}};
+
+		System.out.println("arr="+ Arrays.toString(arr));
+		System.out.println("arr2D="+ Arrays.deepToString(arr2D)); //다차원 배열인 경우
+
+		int[] arr2 = Arrays.copyOf(arr, arr.length);
+		int[] arr3 = Arrays.copyOf(arr, 3);
+		int[] arr4 = Arrays.copyOf(arr, 7); //남은 자리는 0으로 채운다
+		int[] arr5 = Arrays.copyOfRange(arr, 2, 4); //index 범위는 2~3
+		int[] arr6 = Arrays.copyOfRange(arr, 0, 7); //남은 자리는 0으로 채운다
+
+		System.out.println("arr2="+ Arrays.toString(arr2));
+		System.out.println("arr3="+ Arrays.toString(arr3));
+		System.out.println("arr4="+ Arrays.toString(arr4));
+		System.out.println("arr5="+ Arrays.toString(arr5));
+		System.out.println("arr6="+ Arrays.toString(arr6));
+
+		int[] arr7 =  new int[5];
+		Arrays.fill(arr7, 9);  // 모든 값 9로 채우기
+		System.out.println("arr7="+ Arrays.toString(arr7));
+
+		Arrays.setAll(arr7, i -> (int)(Math.random()*6)+1); //채울 값을 함수형 인터페이스나 람다식으로 지정
+		System.out.println("arr7="+ Arrays.toString(arr7));
+
+		for(int i : arr7) {
+			char[] graph = new char[i];
+			Arrays.fill(graph, '*');
+			System.out.println(new String(graph)+i);
+		}
+
+		String[][] str2D  = new String[][]{{"aaa","bbb"},{"AAA","BBB"}};
+		String[][] str2D2 = new String[][]{{"aaa","bbb"},{"AAA","BBB"}};
+
+		System.out.println(Arrays.equals(str2D, str2D2)); //다차원 배열의 경우 배열에 저장된 배열의 주소 값을 비교하게 된다 -> false
+		System.out.println(Arrays.deepEquals(str2D, str2D2)); //true
+
+		char[] chArr = { 'A', 'D', 'C',  'B', 'E' };
+
+		int idx = Arrays.binarySearch(chArr, 'B'); //이진탐색 -> 정렬이 되어 있는 상태여야 한다
+		System.out.println("chArr="+ Arrays.toString(chArr));
+		System.out.println("index of B ="+ Arrays.binarySearch(chArr, 'B'));
+		System.out.println("= After sorting =");
+		Arrays.sort(chArr); //정렬
+		System.out.println("chArr="+ Arrays.toString(chArr));
+		System.out.println("index of B ="+ Arrays.binarySearch(chArr, 'B'));
+	}
+}
